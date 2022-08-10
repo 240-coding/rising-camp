80e3bb035288de42f109edd3ddd12aa4f427c75a
Seoyoung Lee <wow9176@naver.com>
2022-08-10 21:11:08 +0900

    [Init] 1주차 프로젝트 생성

---
+++ Week1/Week1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week1
+//
+//  Created by 이서영 on 2022/08/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

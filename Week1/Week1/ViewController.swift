b5bf64ce8f1b27688a6ccb0e0b240b45efb4a141
Seoyoung Lee <wow9176@naver.com>
2022-08-12 22:11:31 +0900

    [Feat] 버튼들 외곽선 구현

--- Week1/Week1/ViewController.swift
+++ Week1/Week1/ViewController.swift
@@ -9,9 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var pinkView1: UIView!
+    @IBOutlet var pinkView2: UIView!
+    @IBOutlet var pinkView3: UIView!
+    
+    @IBOutlet var moreButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        pinkView1.layer.borderColor = UIColor(named: "pink")?.cgColor
+        pinkView1.layer.borderWidth = 1
+        pinkView1.layer.cornerRadius = pinkView1.frame.height / 2
+        
+        pinkView2.layer.borderColor = UIColor(named: "pink")?.cgColor
+        pinkView2.layer.borderWidth = 1
+        pinkView2.layer.cornerRadius = pinkView2.frame.height / 2
+        
+        pinkView3.layer.borderColor = UIColor(named: "pink")?.cgColor
+        pinkView3.layer.borderWidth = 1
+        pinkView3.layer.cornerRadius = pinkView3.frame.height / 2
+        
+        moreButton.layer.borderWidth = 1
+        moreButton.layer.borderColor = #colorLiteral(red: 0.937254902, green: 0.9333333333, blue: 0.937254902, alpha: 1)
+        
     }
 
 

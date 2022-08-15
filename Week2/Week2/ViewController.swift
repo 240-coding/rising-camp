3de082d5314536317e1202122b84c72dc406d093
Seoyoung Lee <wow9176@naver.com>
2022-08-15 21:02:53 +0900

    [Feat] 내비게이션 컨트롤러, 탭바 컨트롤러 추가

--- Week2/Week2/ViewController.swift
+++ Week2/Week2/ViewController.swift
@@ -12,6 +12,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+         configureNavigationBar()
+    }
+    
+    func configureNavigationBar() {
+        let logoImageView = UIImageView(image: UIImage(named: "logo"))
+        logoImageView.contentMode = .scaleAspectFit
+
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            logoImageView.widthAnchor.constraint(equalToConstant: 30),
+            logoImageView.heightAnchor.constraint(equalToConstant: 30)
+        ])
+                
+        self.navigationItem.titleView = logoImageView
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "EN", style: .plain, target: self, action: nil)
     }
 
 

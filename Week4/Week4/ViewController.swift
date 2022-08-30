1f7cad7aac7ea09f9256e828a61cfa827c25f78a
Seoyoung Lee <wow9176@naver.com>
2022-08-31 05:53:38 +0900

    [Week4] feat: 도구 버튼 관련 프로퍼티 및 메소드 추가

--- Week4/Week4/ViewController.swift
+++ Week4/Week4/ViewController.swift
@@ -8,10 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Properties
+    @IBOutlet var toolButtons: [UIButton]!
+    var selectedTool = -1
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+    }
+    
+    @IBAction func pressedToolButton(_ sender: UIButton) {
+        let index = toolButtons.firstIndex(of: sender)!
+        sender.isSelected.toggle()
+        selectedTool = sender.isSelected ? index : -1
+        setButtonState()
+    }
+    
+    func setButtonState() {
+        for (index, button) in toolButtons.enumerated() {
+            if index == selectedTool {
+                button.layer.borderWidth = 3
+                button.layer.borderColor = UIColor.red.cgColor
+            } else {
+                button.isSelected = false
+                button.layer.borderWidth = 0
+            }
+        }
     }
 
 

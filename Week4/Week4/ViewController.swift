76118f4bc5e4f2bbe0bf9606c928f02c8d3e7594
Seoyoung Lee <wow9176@naver.com>
2022-09-01 05:25:56 +0900

    [Week4] feat: 주문 관련 타이머 구현

--- Week4/Week4/ViewController.swift
+++ Week4/Week4/ViewController.swift
@@ -19,6 +19,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var lifeLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     
+    @IBOutlet var orderView: [UIView]!
+    @IBOutlet var orderLabel: [UILabel]!
+    
+    
+    var mainTimer = Timer()
+    var mainIsRunning = false
+    var mainTimeCount = 0
+    
     var selectedTool = -1 // -1: none, 0: 주전자, 1: 팥, 2: 손
     
     var boongStates = Array(repeating: -1, count: 8) // -1: none, 0: 반죽, 1: 팥, 2: 뒤집기1, 3: 뒤집기2
@@ -27,7 +35,13 @@ class ViewController: UIViewController {
     var boongTimer = Array(repeating: Timer(), count: 8)
     var boongTimeCount = Array(repeating: 0, count: 8)
     
-    var bagCount = 0
+    var orderStatus = [-1, -1] // -1: none, 0: 기다리는 중, 1: 짜증나기 시작
+    var orderBoongAmount = [-1, -1]
+    var orderIsRunning = [false, false]
+    var orderTimer = [Timer(), Timer()]
+    var orderTimeCount = [0, 0]
+    
+    var boongInBagAmount = 0
     
     var life = 3
     var score = 0
@@ -36,8 +50,104 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         bagLabelView.layer.cornerRadius = bagLabelView.frame.height / 2
+        orderView.forEach { view in
+            view.isHidden = true
+        }
+//        startMainTimer()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        startMainTimer()
+    }
+    
+    func startMainTimer() {
+        mainIsRunning = true
+        mainTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(countMainTime), userInfo: nil, repeats: true)
+        let runLoop = RunLoop.current
+        while mainIsRunning {
+            runLoop.run(until: Date().addingTimeInterval(0.1))
+        }
+    }
+    
+    // MARK: - 메인 타이머(주문) 관련
+    @objc func countMainTime() {
+        mainTimeCount += 1
+        let isOrderEnabled = Bool.random()
+        print(isOrderEnabled)
+        if isOrderEnabled && orderStatus[0] == -1 && mainTimeCount % 3 == 0 {
+            print("주문!!")
+            let orderAmount = Int.random(in: 1...6)
+            self.orderBoongAmount[0] = orderAmount
+            DispatchQueue.main.async {
+                self.orderView[0].isHidden = false
+                self.orderLabel[0].text = "붕어빵 \(orderAmount) 주문!"
+            }
+            DispatchQueue.global().async {
+                self.orderStatus[0] = 0
+                self.orderIsRunning[0] = true
+                let runLoop = RunLoop.current
+                self.orderTimer[0] = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.countOrderTime), userInfo: nil, repeats: true)
+                
+                while self.orderIsRunning[0] {
+                    runLoop.run(until: Date().addingTimeInterval(0.1))
+                }
+            }
+        }
+    }
+    
+    @objc func countOrderTime() {
+        orderTimeCount[0] += 1
+        if orderTimeCount[0] >= 15 {
+            orderBoongAmount[0] = -1
+            orderStatus[0] = -1
+            DispatchQueue.main.async {
+                self.orderView[0].backgroundColor = .white
+                self.orderView[0].isHidden = true
+                
+            }
+            self.life -= 1
+            setLife()
+            orderTimeCount[0] = 0
+            orderTimer[0].invalidate()
+            orderIsRunning[0] = false
+        } else if orderTimeCount[0] >= 10 {
+            orderStatus[0] = 1
+            DispatchQueue.main.async {
+                self.orderView[0].backgroundColor = .red
+            }
+        }
+    }
+    
+    func setLife() {
+        if life == 0 {
+            finishGame()
+        }
+        var lifeLabel = ""
+        for _ in 0..<life {
+            lifeLabel += "❤️"
+        }
+        DispatchQueue.main.async {
+            self.lifeLabel.text = lifeLabel
+        }
+    }
+    
+    func finishGame() {
+        mainTimer.invalidate()
+        mainIsRunning = false
+        mainTimeCount = 0
+        for i in 0..<2 {
+            orderTimer[i].invalidate()
+            orderIsRunning[i] = false
+        }
+        for i in 0..<8 {
+            boongTimer[i].invalidate()
+            boongIsRunning[i] = false
+        }
+        print("끝!!!")
+    }
+    
+    // MARK: - 도구 선택 관련
     @IBAction func pressedToolButton(_ sender: UIButton) {
         let index = toolButtons.firstIndex(of: sender)!
         sender.isSelected.toggle()
@@ -57,6 +167,7 @@ class ViewController: UIViewController {
         }
     }
     
+    // MARK: - 붕어빵 관련
     @IBAction func pressedBoongButton(_ sender: UIButton) {
         let index = boongButtons.firstIndex(of: sender)!
         switch selectedTool {
@@ -95,10 +206,8 @@ class ViewController: UIViewController {
             } else if boongStates[index] == 3 { // 2번 뒤집은 후
                 // 붕어빵 상태에 따라 가방에 넣을지 버릴지 달라짐
                 if boongGrillLevel[index] == 1 { // 잘 구운 붕어빵만 봉투에 넣음
-                    bagCount += 1
-                    bagLabel.text = String(bagCount)
-                    score += 10
-                    scoreLabel.text = String(score)
+                    boongInBagAmount += 1
+                    bagLabel.text = String(boongInBagAmount)
                 }
                 initBoongs(index)
             }
@@ -117,7 +226,7 @@ class ViewController: UIViewController {
         boongTimeCount[index] += 1
         if boongTimeCount[index] <= 5 {
             boongGrillLevel[index] = 0
-        } else if boongTimeCount[index] <= 12 {
+        } else if boongTimeCount[index] <= 18 {
             boongGrillLevel[index] = 1
         } else {
             boongGrillLevel[index] = 2
@@ -127,7 +236,6 @@ class ViewController: UIViewController {
                 }
             }
         }
-        print(boongTimeCount[index])
     }
     
     func initBoongs(_ index: Int) {
@@ -138,6 +246,31 @@ class ViewController: UIViewController {
         boongGrillLevel[index] = 0
         boongTimeCount[index] = 0
     }
-
+    
+    // MARK: - 봉투 관련
+    
+    @IBAction func pressedBagButton() {
+        if boongInBagAmount == orderBoongAmount[0] {
+            print("성공!")
+            score += 10
+            scoreLabel.text = String(score)
+            initOrders()
+            initBag()
+        }
+    }
+    
+    func initOrders() {
+        orderView[0].isHidden = true
+        orderStatus[0] = -1
+        orderBoongAmount[0] = -1
+        orderIsRunning[0] = false
+        orderTimeCount[0] = 0
+        orderTimer[0].invalidate()
+    }
+    
+    func initBag() {
+        boongInBagAmount = 0
+        bagLabel.text = "0"
+    }
 
 }

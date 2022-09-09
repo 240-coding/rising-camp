8ba9b8b500206bf766ff1b9b0e7f3724595e0dbf
Seoyoung Lee <wow9176@naver.com>
2022-09-09 22:31:23 +0900

    [Week5] feat: 탭 내비게이션 초기 설정 및 그라데이션 배경 구현

--- Week5/Week5/ViewController.swift
+++ Week5/Week5/ViewController.swift
@@ -8,10 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var gradientView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        gradientView.setGradient()
     }
 
 

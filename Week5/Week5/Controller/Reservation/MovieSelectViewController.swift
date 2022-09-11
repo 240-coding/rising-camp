ead2c1fc655fc32eaf31c72f10af1396e2c869e0
Seoyoung Lee <wow9176@naver.com>
2022-09-12 07:57:04 +0900

    [Week5] feat: 영화 선택 화면 데이터 불러오기

--- Week5/Week5/Controller/Reservation/MovieSelectViewController.swift
+++ Week5/Week5/Controller/Reservation/MovieSelectViewController.swift
@@ -13,16 +13,15 @@ private let reuseIdentifier = "reusableView"
 class MovieSelectViewController: UIViewController {
     
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    var movieTitle = [String]()
 
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setNavigationItem()
         collectionView.register(UINib(nibName: "MovieCollectionReusableView", bundle: nil), forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: reuseIdentifier)
-        
-    }
-    
-    override func viewDidLayoutSubviews() {
-//        configureSearchBar()
+        fetchBoxOfficeData()
     }
     
     func setNavigationItem() {
@@ -49,12 +48,28 @@ class MovieSelectViewController: UIViewController {
     
     @objc func closeViewController() {
         dismiss(animated: true)
-    }    
+    }
+    
+    // MARK: - Networking
+    func fetchBoxOfficeData() {
+        MovieInfo.shared.requestBoxofficeData { result in
+            switch result {
+            case .success(let data):
+                for movie in data.boxOfficeResult.dailyBoxOfficeList {
+                    self.movieTitle.append(movie.movieNm)
+                    self.collectionView.reloadData()
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 }
 
+// MARK: - UICollectionView
 extension MovieSelectViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return movieTitle.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -62,15 +77,30 @@ extension MovieSelectViewController: UICollectionViewDelegate, UICollectionViewD
             return UICollectionViewCell()
         }
         
+        MovieInfo.shared.requestAdditionalMovieData(query: movieTitle[indexPath.row]) { result in
+            switch result {
+            case .success(let data):
+                DispatchQueue.main.async {
+                    if let url = URL(string: data.items[0].image) {
+                        cell.posterImageView.load(url: url)
+                    }
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+        cell.titleLabel.text = movieTitle[indexPath.row]
+        
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let width = collectionView.frame.width / 3 - 10
+        let width = collectionView.frame.width / 3 - 16
         let height: CGFloat = 240
         return CGSize(width: width, height: height)
     }
     
+    // Header
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         switch kind {
         case UICollectionView.elementKindSectionHeader:
